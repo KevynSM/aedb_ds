d6647f2ba99c13a2f7de67ba2dcd107f1031f7ea
Kevyn Marins <kevynmarins@gmail.com>
2020-05-21 13:25:06 +0100

    marge hash_table 2.0

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -1,35 +1,6 @@
 from .tad_dictionary import Dictionary
 from ..exceptions import NoSuchElementException, DuplicatedKeyException
 from ..lists.singly_linked_list import SinglyLinkedList
-<<<<<<< HEAD
-from .item import Item
-import ctypes
-
-class HashTable(Dictionary):
-    def __init__(self, size=101):       
-       self.elements = 0
-       self.max_elements = size
-       idx_array = (self.max_elements * ctypes.py_object)()
-
-    def size(self):
-        return self.elements
-
-    def is_full(self):
-        return self.max_elements == self.elements
-
-    def get(self, k): pass
-
-
-    def insert(self, k, v): 
-        idx = hash(k,self.max_elements)
-        if type(k) == "int":
-            idx = k % self.max_elements
-        elif type(k) == "str":
-            idx = sum(ord(c))
-        new_item = Item(k,v)
-        idx_array[idx] = 
-
-=======
 from .item import Item 
 
 import ctypes 
@@ -66,7 +37,6 @@ class HashTable(Dictionary):
         self.table[idx].insert_last(item)
         # Update the number of elements
         self.num_elements += 1    
->>>>>>> upstream/develop
 
     def update(self, k, v): pass
 
